b8b2a3a3bb7602323419efc0fb6b6046edcc242c
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-12-05 21:46:20 +0100

    no need for error-handling

--- source/syscall.s
+++ source/syscall.s
@@ -1,20 +1,10 @@
 .intel_syntax noprefix
 
 .text
-.global _syscall
+.globl _syscall
 
 _syscall:
     xor rax, rax
     mov r10, rcx
     syscall
-    jb err
-    jmp exit
-err:
-    push rax
-    call __error
-    pop rcx
-    mov [rax], ecx
-    mov rax, 0xFFFFFFFFFFFFFFFF
-    mov rdx, 0xFFFFFFFFFFFFFFFF
-exit:
     ret
